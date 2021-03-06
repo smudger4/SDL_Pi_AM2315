0aa0b1d05d844ccd592fb1eedc15b253b90f50ee
Nick Smith <nick.db.smith@mac.com>
2021-03-06 13:05:55 +0000

    comments

--- watchdog.sh
+++ watchdog.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-# Set indigo credentials in /etc/environment
+# watchdog script to check to see if the sensor service has died
+# if so, likely that sensor needs a power-cycle to recover
+# ask home automation to power-cycle the system
+# home automation command has a delay built in to allow time for a clean shutdown
 
-# uncomment curl & halt lines to activate
+# Set Indigo credentials in /etc/environment
+# copy to /usr/local/bin as root before adding to root crontab
+# active lines commented out for safety - uncomment in version run by crontab
 
 if ! (/usr/bin/systemctl -q is-active awsiot_service) then
     /usr/bin/logger "[watchdog] awsiot_service is inactive - halting"
-    #/usr/bin/curl -X EXECUTE -u "$INDIGO_USER:$INDIGO_PASSWORD" --digest http://russ.home:8176/actions/reset-garden-sensor-delayed
+    #/usr/bin/curl -X EXECUTE -u "$INDIGO_USER:$INDIGO_PASSWORD" --digest $INDIGO_URL
     #/usr/sbin/halt
 fi
