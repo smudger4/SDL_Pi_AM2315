078173c3732f8c2a34e793b786a064bf2e21d8dc
Nick Smith <nick.db.smith@mac.com>
2021-03-06 12:45:29 +0000

    fix service name

--- watchdog.sh
+++ watchdog.sh
@@ -3,7 +3,7 @@
 
 # uncomment curl & halt lines to activate
 
-if ! (/usr/bin/systemctl -q is-active awsiot-service) then
+if ! (/usr/bin/systemctl -q is-active awsiot_service) then
     /usr/bin/logger "[watchdog] awsiot_service is inactive - halting"
     #/usr/bin/curl -X EXECUTE -u "$INDIGO_USER:$INDIGO_PASSWORD" --digest http://russ.home:8176/actions/reset-garden-sensor-delayed
     #/usr/sbin/halt
