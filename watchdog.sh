794292a22c4f563be4d9af1f2fe5b33eaf8c7108
Nick Smith <nick.db.smith@mac.com>
2021-03-06 09:48:16 +0000

    added full paths

--- watchdog.sh
+++ watchdog.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# Set indigo credentials in environment
+# Set indigo credentials in /etc/environment
 
 # uncomment curl & halt lines to activate
 
-if ! (systemctl -q is-active awsiot-service) then
-    logger "[watchdog] awsiot_service is inactive - halting"
-    #curl -X EXECUTE -u "$INDIGO_USER:$INDIGO_PASSWORD" --digest http://russ.home:8176/actions/reset-garden-sensor-delayed
-    #halt
+if ! (/usr/bin/systemctl -q is-active awsiot-service) then
+    /usr/bin/logger "[watchdog] awsiot_service is inactive - halting"
+    #/usr/bin/curl -X EXECUTE -u "$INDIGO_USER:$INDIGO_PASSWORD" --digest http://russ.home:8176/actions/reset-garden-sensor-delayed
+    #/usr/sbin/halt
 fi
